8c9c06b6ded711d230f978c3feb63f0befc927ef
Li Ke 李可 <V_like01@saicmotor.com>
2019-07-26 14:41:16 +0800

    chatEngine第一次上传

---
+++ app/src/main/java/com/chatengine/MainActivity.java
@@ -0,0 +1,67 @@
+package com.chatengine;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private List<Msg> msgList = new ArrayList<Msg>();
+
+    private EditText inputText;
+
+    private Button send;
+
+    private RecyclerView msgRecyclerView;
+
+    private MsgAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initMsgs(); // 初始化消息数据
+        inputText = (EditText) findViewById(R.id.input_text);
+        send = (Button) findViewById(R.id.send);
+        msgRecyclerView = (RecyclerView) findViewById(R.id.msg_recycle_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        msgRecyclerView.setLayoutManager(layoutManager);
+        adapter = new MsgAdapter(msgList);
+        msgRecyclerView.setAdapter(adapter);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = inputText.getText().toString();
+                if (!"".equals(content)) {
+                    Msg msg = new Msg(content, Msg.TYPE_SENT);
+                    msgList.add(msg);
+                    // 当有新消息时，刷新ListView中的显示
+                    adapter.notifyItemInserted(msgList.size() - 1);
+                    // 将ListView定位到最后一行
+                    msgRecyclerView.scrollToPosition(msgList.size() - 1);
+                    // 清空输入框中的内容
+                    inputText.setText("");
+                }
+            }
+        });
+    }
+
+    /**
+     * 初始化聊天消息
+     */
+    private void initMsgs() {
+        Msg msg1 = new Msg("在么？", Msg.TYPE_RECEIVED);
+        msgList.add(msg1);
+        Msg msg2 = new Msg("不在，CNM ", Msg.TYPE_SENT);
+        msgList.add(msg2);
+        Msg msg3 = new Msg("*龙门粗口* ", Msg.TYPE_RECEIVED);
+        msgList.add(msg3);
+    }
+}
