e6c76680d490f6a37122911210f62332add5d1b5
saiofo <likepnd@sina.com>
2019-08-02 16:56:16 +0800

    代码格式整理、输入内容处理功能开发

---
+++ app/src/main/java/com/chatengine/messageOrganizer/ContentHandle.java
@@ -0,0 +1,11 @@
+package com.chatengine.messageOrganizer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContentHandle {
+    public List<String> searchTraget(String str, String target){
+        List<String> strs = new ArrayList<String>();
+        return strs;
+    }
+}
